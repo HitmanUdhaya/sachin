f90518dd0122f192cb2b8e8b4b7f1ce299d46c9b
HitmanUdhaya <udhaya.mohan1995@gmail.com>
2024-02-22 19:45:31 +0530

    Done

---
+++ src/git/Git.java
@@ -0,0 +1,14 @@
+package git;
+
+public class Git {
+public void tc1() {
+	System.out.println("tc1");
+	
+}
+public void tc2() {
+	System.out.println("tc2");
+}
+public void tc3() {
+	System.out.println("tc3");
+}
+}
